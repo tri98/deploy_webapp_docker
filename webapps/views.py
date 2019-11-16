ddf104dcf2d236a47658958a54305b6c4855340e
tribvt <buivantritbv@gmail.com>
2019-11-16 14:00:21 +0700

    Update layout.

--- webapps/views.py
+++ webapps/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'base.html')
+
+def newProject(request):
+    return render(request, 'project.html')
